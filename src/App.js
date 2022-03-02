9d63e619c0695a1824a765b76084e46bfaccd1ba
neilwiborg <NeilWiborg@outlook.com>
2022-03-02 13:39:00 -0800

    add React files

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Time from './Time';
+import date from 'date-and-time';
+
+function App() {
+  const now = new Date();
+
+  const later = date.addHours(now, -1);
+  return (
+    <div className="row">
+      <div className="column">
+        <h1>
+          Pacific Standard Time:
+        </h1>
+        <h2>
+          {date.format(now, 'hh:mm A')}
+        </h2>
+      </div>
+      <div className="column">
+        <h1>
+          Safwaan Standard Time:
+        </h1>
+        <h2>
+          {date.format(later, 'hh:mm A')}
+        </h2>
+      </div>
+    </div>
+  );
+}
+export default App;

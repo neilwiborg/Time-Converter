dd411ba55ffe4e411ada0e722cc4dc86d6eb67c4
neilwiborg <NeilWiborg@outlook.com>
2022-07-23 13:23:16 -0700

    start project from scratch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Time from './Time';
-import date from 'date-and-time';
 
 function App() {
-  const now = new Date();
-
-  const later = date.addHours(now, -1);
   return (
-    <div className="App-header">
-      <div className="left">
-        <h1>
-          Pacific Standard Time:
-        </h1>
-        <h2>
-          {date.format(now, 'hh:mm A')}
-        </h2>
-      </div>
-      <div className="right">
-        <h1>
-          Safwaan Standard Time:
-        </h1>
-        <h2>
-          {date.format(later, 'hh:mm A')}
-        </h2>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
 export default App;
